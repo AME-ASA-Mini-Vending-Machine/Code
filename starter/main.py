753e48fafc0958fbb53464c3682124562b42003d
AME-ASA-Mini-Vending-Machine <sumedhcam@gmail.com>
2025-06-15 11:44:21 -0400

    Update main.py

--- starter/main.py
+++ starter/main.py
@@ -2,19 +2,25 @@ from DIYables_Pico_Keypad import Keypad
 import time
 from machine import I2C, Pin, PWM
 from I2C_LCD import I2CLcd
+from micropython_servo_pdm_360 import ServoPDM360
 
 
 # corresponds to 3x4 keypad layout
-KEYMAP = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "*", "0", "#"]
+KEYMAP = ["1", "2", "3",
+          "4", "5", "6",
+          "7", "8", "9",
+          "*", "0", "#"]
 
-COSTMAP = [0, 1, 1, 1, 2, 2, 2, 3, 3, 3]
+COSTMAP = [1, 1, 1,
+           2, 2, 2,
+           3, 3, 3]
 
 
-# initialize i2c protocol
-i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
+# Initialize I2C protocol
+i2c = I2C(0, sda = Pin(0), scl = Pin(1), freq = 400000)
 devices = i2c.scan()
 
-# initialize lcd
+# Initialize LCD
 try:
     if devices != []:
         lcd = I2CLcd(i2c, devices[0], 2, 16)
@@ -24,36 +30,36 @@ try:
 except:
     print("Error initializing LCD.")
 
-# initialize keypad
+# Initialize keypad
 # keymap, number of row pins, GPIO pins of row pins, GPIO pins of column pins
-keypad = Keypad(
-    KEYMAP, ROW_PINS=4, COLUMN_PINS=3, NUM_ROWS=[3, 4, 5, 6], NUM_COLS=[0, 1, 2]
-)
-# >400 ms between each button press
+keypad = Keypad(KEYMAP, ROW_PINS = [3, 4, 5, 6], COLUMN_PINS = [0, 1, 2], NUM_ROWS = 4, NUM_COLS = 3)
+
+# > 400 ms between each button press
 keypad.set_debounce_time(400)
 
-# initialize ir sensor
-ir_sensor = Pin(17, Pin.IN, Pin.PULL_DOWN)
+# Initialize IR sensor
+ir = Pin(17, Pin.IN, Pin.PULL_DOWN)
 
-# intialize servos with pins 0-9
+# Initialize servos with pins 0-9
 servos = [PWM(Pin(i)) for i in range(10)]
 
-# Set Duty Cycle for Different Speeds
-max_duty = 7864  # max cw rotation
-min_duty = 1802  # max ccw rotation
-half_duty = int((max_duty - min_duty) / 2)  # stop rotation
+# Set duty cycle
+max_duty = 65535
+min_duty = 0
+half_duty = int((max_duty - min_duty) / 2)
 
-# vending machine state variables
+# Vending machine state variables
 queue = ""
 confirmed = False
 paid = False
 cost = 1000
 balance = 0
 
-#function to read keys from keypad and add it to a queue, press # to finish reading keys
+# Function to read keys from keypad and add them to a queue, press # to finish reading keys
 def read_key():
-    global confirmed, cost,queue
-    if confirmed==False:
+    global confirmed, cost, queue
+    
+    if confirmed == False:
         key = keypad.get_key()
         if key in "123456789":
             queue += key
@@ -66,17 +72,21 @@ def read_key():
                 time.sleep(1)
                 queue = ""
 
-#function to display text on LCD screen based on state of vending machine
+# Function to display text on LCD screen based on state of vending machine
 def display():
-    global queue,confirmed,paid,cost,balance
+    global queue, confirmed, paid, cost, balance
+    
     if len(queue) == 0:
         lcd.putstr("Enter key for candy. Press # when done.")
+    
     elif confirmed == False:
         lcd.putstr(queue)
+    
     elif paid == False:
         lcd.putstr("Required: " + cost + "\nBalance: " + balance)
+    
     else:
-        lcd.pustr("Enjoy your snack.")
+        lcd.pustr("Enjoy your snack!")
         time.sleep(5)
         queue = ""
         confirmed = False
@@ -84,23 +94,23 @@ def display():
         cost = 1000
         balance = 0
 
-#function to detect inputted coins
+# Function to detect inputted coins
 def read_coin():
     global balance
+    
     val = ir_sensor.value()
     if val == 0:
         balance += 1
         time.sleep(0.1)
 
-#function to dispense queued item after cost is paid
+# Function to dispense queued item after cost is paid
 def dispense_item():
     global paid
+    
     if balance >= cost:
         paid = True
-        servos[int(queue) - 1].duty_u16(max_duty)
+        servos[int(queue) - 1].turn_cv()
         time.sleep(0.4)
-        servos[int(queue) - 1].duty_u16(half_duty)
-
 
 # Main loop
 while True:
